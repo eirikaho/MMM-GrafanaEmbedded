a54e37b0f39e21efff91a231a6254db9513a00ec
Eirik Auglænd Holst <eirikaho@outlook.com>
2021-03-27 10:04:03 +0100

    first commit

---
+++ MMM-GrafanaEmbedded.js
@@ -0,0 +1,30 @@
+/* MMM-GrafanaEmbedded
+ * This MagicMirror² module allows you to embed grafana panel.
+ *
+ * By Eirik Holst
+ * MIT Licensed.
+ */
+
+Module.register("MMM-GrafanaEmbedded", {
+    // Default module config.
+    defaults: {
+        height:"200",
+        width:"450"
+    },
+
+    // Define start sequence.
+    start: function() {
+        Log.info("Starting module: " + this.name);
+    },
+    // Override dom generator.
+    getDom: function() {
+        var iframe = document.createElement("IFRAME");
+        const {host, port, id, orgId, dashboardName, refreshRate, from, to, panelId, width, height} = this.config;
+        // todo: README time range controls https://grafana.com/docs/grafana/latest/dashboards/time-range-controls
+        iframe.width = width;
+        iframe.height = height;
+        iframe.frameborder = "0"
+        iframe.src =  `http://${host}:${port}/d-solo/${id}/${dashboardName}?kiosk=&orgId=${orgId}&refresh=${refreshRate}&from=${from}&to=${to}&panelId=${panelId}`;
+        return iframe;
+    }
+});
---
+++ README.md
@@ -0,0 +1,38 @@
+# MMM-GrafanaEmbedded [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)]
+
+Embed panels from grafana (requires grafana v6 or higher)
+
+## Example
+
+![](others/MMM-SystemStatus.png)
+
+## Installatoion
+1. `cd ~/MagicMirror/modules`
+1. `git clone https://github.com/eirikaho/MMM-SystemStatus.git`
+1. `cd MMM-SystemStatus`
+1. `npm install`
+1. Configure `~/MagicMirror/config/config.js`:
+
+    ```
+		{
+			module: 'MMM-GrafanaEmbedded',
+			position: 'top_right',
+			config: {
+				id: "",
+				host: "",
+				port: ""
+				dashboardName: "",
+				orgId: ,
+				panelId: ,
+				width: "450",
+				height: "200",
+				refreshRate: "5m",
+				from: "now-24h",
+				to: "now"
+			}
+		},
+    ```
+url composition example
+http://<host>:<port>/d/<id>/>dashboardName>?orgId=<orgId>&refresh=<refreshRate>&from=<from>&to=<to>&panelId=<panelId>
+
+for time range syntax (from, to) see [grafana time range controls](https://grafana.com/docs/grafana/latest/dashboards/time-range-controls/)
